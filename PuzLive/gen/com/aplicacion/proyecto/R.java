d0c2e3c185bff5674d3d5ad256b2d9e8b195f929
cacaicedo <cacaiced@espol.edu.ec>
2012-11-17 13:42:51 -0500

    Agregue el Acerca de

--- PuzLive/gen/com/aplicacion/proyecto/R.java
+++ PuzLive/gen/com/aplicacion/proyecto/R.java
@@ -11,22 +11,29 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int fondo=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int colorfondo=0x7f020000;
+        public static final int fondo=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070006;
-        public static final int tvAcercaDe=0x7f070002;
-        public static final int tvDificil=0x7f070005;
-        public static final int tvFacil=0x7f070003;
-        public static final int tvMedio=0x7f070004;
-        public static final int tvPartida=0x7f070000;
-        public static final int tvRanking=0x7f070001;
+        public static final int imageView1=0x7f070004;
+        public static final int menu_settings=0x7f07000b;
+        public static final int textView1=0x7f070000;
+        public static final int textView2=0x7f070001;
+        public static final int textView3=0x7f070002;
+        public static final int textView4=0x7f070003;
+        public static final int tvAcercaDe=0x7f070007;
+        public static final int tvDificil=0x7f07000a;
+        public static final int tvFacil=0x7f070008;
+        public static final int tvMedio=0x7f070009;
+        public static final int tvPartida=0x7f070005;
+        public static final int tvRanking=0x7f070006;
     }
     public static final class layout {
-        public static final int activity_puz_live=0x7f030000;
-        public static final int niveles=0x7f030001;
+        public static final int acerca_de=0x7f030000;
+        public static final int activity_puz_live=0x7f030001;
+        public static final int niveles=0x7f030002;
     }
     public static final class menu {
         public static final int activity_puz_live=0x7f060000;
